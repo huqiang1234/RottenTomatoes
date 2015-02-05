d9badde93b0e4e255e0fcbd3ae14975ed17f7326
Charlie Hu <charlie@box.com>
2015-02-04 17:51:52 -0800

    Initial Commit

---
+++ RottenTomatoes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RottenTomatoes
+//
+//  Created by Charlie Hu on 2/4/15.
+//  Copyright (c) 2015 Charlie Hu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
